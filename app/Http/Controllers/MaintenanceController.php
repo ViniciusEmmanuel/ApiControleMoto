83b889cdca7ff69041dad83599a500ad3eaa2fe7
Vinicius Emmanuel <vinicius.teixeira.m@gmail.com>
2020-04-25 16:39:38 -0300

    Controller: Maintenance, query all formated

--- app/Http/Controllers/MaintenanceController.php
+++ app/Http/Controllers/MaintenanceController.php
@@ -6,7 +6,6 @@ use App\Models\Gasoline;
 use App\Models\Maintenance;
 use App\Services\AuthJwt;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use phpDocumentor\Reflection\Types\Object_;
 
 class MaintenanceController extends Controller
@@ -17,30 +16,10 @@ class MaintenanceController extends Controller
 
         $dateAfter = $request->get('date_after');
         $dateBefore = $request->get('date_before');
-        $partId = $request->get('part_id');
         $motorcicleId = $request->get('motorcicle_id');
+        $partId = $request->get('part_id');
 
-        $where = ' ' . 'where 1 = ?';
-
-        $binds = [1];
-
-        if ($partId) {
-            $where .= ' ' . 'and vm.part_id = ?';
-            array_push($binds, (int) $partId);
-        }
-
-        if ($dateAfter && $dateBefore) {
-            $where .= ' ' . 'and vm.date between date(?) and date(?)';
-            array_push($binds, $dateBefore);
-            array_push($binds, $dateAfter);
-        }
-
-        if ($motorcicleId) {
-            $where .= ' ' . 'and vm.motorcicle_id = ?';
-            array_push($binds, $motorcicleId);
-        }
-
-        $maintenanceLists = DB::select("select * from view_maintenance vm" . $where, $binds);
+        $maintenanceLists = Maintenance::findFormatTable($dateBefore, $dateAfter, $motorcicleId, $partId);
 
         return $this->createResponse('success', $maintenanceLists, 200);
     }
--- app/Models/Maintenance.php
+++ app/Models/Maintenance.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Maintenance extends Model
 {
@@ -30,5 +31,78 @@ class Maintenance extends Model
     protected $mechanic;
 
     protected $deleted;
+
+    public static function findFormatTable($dateBefore, $dateAfter, $motorcicleId, $partId)
+    {
+        $where = ' ' . 'where 1 = ?';
+
+        $binds = [1];
+
+        if ($dateAfter && $dateBefore) {
+            $where .= ' ' . 'and x.date between date(?) and date(?)';
+            array_push($binds, $dateBefore);
+            array_push($binds, $dateAfter);
+        }
+
+        if ($motorcicleId) {
+            $where .= ' ' . 'and x.motorcicle_id = ?';
+            array_push($binds, $motorcicleId);
+        }
+
+        if ($partId) {
+            $where .= ' ' . 'and x.part_id = ?';
+            array_push($binds, (int) $partId);
+        }
+
+        $where .= ' ';
+
+        return DB::select(
+            "SELECT
+                x.id,
+                x.date,
+                x.motorcicle_id,
+                x.board,
+                x.part_id,
+                x.name,
+                x.km_last,
+                x.km,
+                x.km_per_run,
+                x.price,
+                x.created_at,
+                x.updated_at
+            from
+                (
+                SELECT
+                    IF (@motoId <> m.motorcicle_id or @partId <> m.part_id, @kmlast := 0, NULL),
+                    m.id,
+                    m.date,
+                    m.motorcicle_id,
+                    mc.board ,
+                    m.part_id,
+                    p.name,
+                    @kmlast as km_last,
+                    m.km,
+                    CASE
+                        when @kmlast = 0
+                            then 0
+                        else
+                            (m.km - @kmlast)
+                    end as km_per_run,
+                    m.mechanic,
+                    m.price,
+                    m.created_at,
+                    m.updated_at,
+                    @kmlast := m.km,
+                    @motoId := m.motorcicle_id,
+                    @partId := m.part_id
+                from
+                    maintenance m
+                join motorcicles mc on
+                    (mc.id = m.motorcicle_id)
+                join parts p on
+                    (p.id = m.part_id)
+                order by m.motorcicle_id, m.part_id, m.date, m.id
+            ) x " . $where . "ORDER by x.motorcicle_id, x.part_id, x.date desc, x.id desc", $binds);
+    }
 
 }
